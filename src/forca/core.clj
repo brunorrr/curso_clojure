8f49663cda0407da6cc9c627eee8f886f22306f1
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-10-14 22:46:00 -0300

    Função Fibonacci

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -25,6 +25,19 @@
   )
 )
 
+(defn fib [n] 
+  ( if (= n 0) 
+    0
+    ( if (= n 1) 
+      1
+      (+ 
+        (fib (- n 1) ) 
+        (fib (- n 2) ) 
+      )
+    )
+  )
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
